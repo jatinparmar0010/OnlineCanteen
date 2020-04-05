c1ba340bf4745000878f7f9147be476d38d20376
jatinparmar0010 <58782055+jatinparmar0010@users.noreply.github.com>
2020-04-05 22:08:23 +0530

    Add files via upload

---
+++ OnlineCanteen/App_Code/Connection_String.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using MySql.Data.MySqlClient;
+
+/// <summary>
+/// Summary description for Connection_String
+/// </summary>
+public class Connection_String
+{
+    public static MySqlConnection con = null;
+    public MySqlConnection Connect_Database()
+	{
+        string constr = "server=localhost;user=root;database=website1;port=3306;password=root";
+        con = new MySqlConnection(constr);
+        con.Open();
+        return con;
+        
+	}
+}
